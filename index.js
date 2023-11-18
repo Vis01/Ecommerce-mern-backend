77dd569ac696e3c9c78a62a80836e220beae03ed
Vishnu Jangid <vishnu.j8503@gmail.com>
2023-11-18 11:54:07 +0530

    for data

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ server.use(
 );
 server.use(express.json()); // to parse req.body
 
-server.use('/products', isAuth(), productsRouter.router);
+server.use('/products', productsRouter.router); //, isAuth()
 // we can also use JWT token for client-only auth
 server.use('/categories', isAuth(), categoriesRouter.router);
 server.use('/brands', isAuth(), brandsRouter.router);
